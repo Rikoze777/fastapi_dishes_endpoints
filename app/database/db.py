e8af80f4968e92a0c4bbb824105a9a0f187ca53b
Rikoze777 <spawn00007@gmail.com>
2024-02-04 17:40:08 +0300

    add redis connection

--- app/database/db.py
+++ app/database/db.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.config import Config
+import redis
 
 
 config = Config()
@@ -8,6 +9,11 @@ database_url = config.POSTGRES_URL
 engine = create_engine(database_url)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_redis():
+    cache = redis.Redis(host='redis', port=6379)
+    return cache
 
 
 def get_db():
